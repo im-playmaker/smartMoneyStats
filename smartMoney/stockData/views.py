25b248ddaddcea73b59052d1c7e818068ce090d4
Seifeddin abassi <abassi.saif.ns@gmail.com>
2024-12-04 16:19:38 +0100

    read data from database and show it in html

--- smartMoney/stockData/views.py
+++ smartMoney/stockData/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Stock
 
 # Create your views here.
+def stock(request):
+    items = Stock.objects.all()
+    return render(request, "stock.html", {"stocks": items})
